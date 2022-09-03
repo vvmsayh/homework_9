88ca26fe295db6242a1a6f0f62ab943e826c5303
unknown <radivoniknikita@gmail.com>
2022-09-03 21:29:51 +0300

    ДЗ

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+string Numbers(int start,int end)
+{
+    //базовый случай
+    if(start==end) return start.ToString();
+    // Рекурсивный случай
+    return (start + ", " + Numbers(start + 1, end));
+}
+Console.WriteLine($"Числа от {M} до {N}: [{Numbers(M,N)}]");
